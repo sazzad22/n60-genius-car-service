d6e2a23bb4926ccf4ec542332a7e2f901224974c
Sazzad ur Rahman <22joylobo@gmail.com>
2022-04-16 12:19:01 +0600

    Home page components created

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Experts from '../../Experts/Experts';
+import Banneer from '../Banner/Banneer';
 import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
             <h1 className='text-primary'>This is home</h1>
-            
+            <Banneer></Banneer>
             <Services></Services>
             <Experts></Experts>
         </div>
