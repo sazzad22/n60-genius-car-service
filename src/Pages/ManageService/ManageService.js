dadf0bfc2e8efe3b4b39013b130cd8f150e79582
Sazzad ur Rahman <22joylobo@gmail.com>
2022-04-26 12:40:24 +0600

    notes

--- src/Pages/ManageService/ManageService.js
+++ src/Pages/ManageService/ManageService.js
@@ -7,12 +7,13 @@ const ManageService = () => {
   const handleDelete = (id) => {
     const proceed = window.confirm("Are you sure");
     if (proceed) {
-      const url = `http://localhost:5000/service/${id}`;
+      const url = `http://localhost:5000/services/${id}`;
       fetch(url, {
         method: "DELETE",
       })
         .then((res) => res.json())
         .then((data) => {
+          console.log(data);
           const remaining = services.filter((service) => service._id !== id);
           setServices(remaining);
         });
@@ -24,7 +25,8 @@ const ManageService = () => {
       {services.map((service) => (
         <div key={service._id}>
           <h5>
-            {service.name} <button>X</button>
+            {service.name}{" "}
+            <button onClick={() => handleDelete(service._id)}>X</button>
           </h5>
         </div>
       ))}
